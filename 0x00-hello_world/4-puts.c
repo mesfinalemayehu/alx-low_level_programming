7d99bb99a0f3c3e830f6c5a2363e0e2405e19c83
mesfinalemayehu <mesfinalemayehu2519@gmail.com>
2022-12-10 08:34:34 -0800

    task05

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,8 @@
 #include<stdio.h>
+/**
+ * this iis main entry point
+ * and it print the text to the screen
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+/* *
+ * this is the main entery point 
+ *it print a text to the screen
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n";
+	return (0);
+}
