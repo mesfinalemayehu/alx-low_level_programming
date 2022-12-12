5b39dc6868d7ec0e70f08e62163595c57741e13b
mesfinalemayehu <mesfinalemayehu2519@gmail.com>
2022-12-11 21:20:30 -0800

    task101

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,13 +1,15 @@
-#include<stdio.h>                                                                     
-/**                                                                                   
- *main - Entry point                                                                  
- *Return: Always 0 (success)                                                          
- */                                                                                   
-int main(void)                                                                  {                                                                                      
-        puts("Size of a char: %zu byte(s)\n", sizeof(char));                  
-        puts("Size of an int: %zu byte(s)\n", sizeof(int));                  
-        puts("Size of a long int: %zu byte(s)\n", sizeof(long int));  
-        puts("Size of a long long int: %zu byte(s)\n", sizeof(long long int));   
-	puts("Size of a float: %zu byte(s)\n", sizeof(float));                  
-        return (0);      
-}  
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	printf("Size of a char: %1d byte(s)\n", sizeof(char));
+	printf("Size of an int: %1d byte(s)\n", sizeof(int));
+	printf("Size of a long int: %1d byte(s)\n", sizeof(long int));
+	printf("Size of a long long int: %1d byte(s)\n", sizeof(long long int));
+	printf("Size of a float: %1d byte(s)\n", sizeof(float));
+	return (0);
+}
