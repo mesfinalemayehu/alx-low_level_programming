921aaf261ce2162c06f9b68f3dbe8382c3c95582
mesfinalemayehu <mesfinalemayehu2519@gmail.com>
2022-12-16 05:37:36 -0800

    taskdone

---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,18 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+
+void positive_or_negative(int i);
+
+int _isupper(int c);
+int mul(int a, int b);
+int _isdigit(int c);
+void print_numbers(void);
+void print_most_numbers(void);
+void more_numbers(void);
+void print_line(int n);
+void print_diagonal(int n);
+void print_triangle(int size);
+void print_square(int size);
+#endif /* MAIN_H */ 
